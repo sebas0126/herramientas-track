5c587936eca078c05cbf18f8cee700e72696560a
Jhojan David Guerero Medina <jhojan.guerrero@MacBook-Pro-de-doyle.local>
2017-06-12 17:11:42 -0500

    Actualización de maquetación

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,21 @@
 (function() {
-  $(document).ready(function() {});
+  $(document).ready(function() {
+    var path, target;
+    $('.background').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+    path = window.location.pathname.split('/').pop();
+    if (path === '') {
+      path = 'index.html';
+    }
+    target = $('.nav li a[href="' + path + '"]');
+    return target.addClass('active');
+  });
 
 }).call(this);
 
--- src/js/main.coffee
+++ src/js/main.coffee
@@ -6,3 +6,16 @@
 # Company : BIG (http://www.bigholding.com.co)
 
 $(document).ready ->
+	$('.background').slick
+		autoplay: true
+		autoplaySpeed: 4000
+		dots: true
+		infinite: true
+		slidesToShow: 1
+		adaptiveHeight: true
+
+	path = window.location.pathname.split('/').pop()
+	if path == ''
+	  path = 'index.html'
+	target = $('.nav li a[href="' + path + '"]')
+	target.addClass 'active'
